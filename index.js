998ee9a1bb6a45bfed376fd6f302d5ba420590ff
LahcenHaouch <lahcen.haouch@protonmail.com>
2026-08-20 22:10:23 +0200

    Display new book modal

--- index.js
+++ index.js
@@ -1,44 +1,32 @@
+function Book(title, author, year) {
+  if (!new.target) {
+    throw new Error("Constructor function must be called with new.");
+  }
+
+  this.id = crypto.randomUUID();
+  this.title = title;
+  this.author = author;
+  this.year = year;
+}
+
 const LIBRARY = [
-  {
-    title: "1984",
-    author: "George Orwell",
-    year: 1949,
-  },
-  {
-    title: "The Great Gatsby",
-    author: "F. Scott Fitzgerald",
-    year: 1925,
-  },
-  {
-    title: "To Kill a Mockingbird",
-    author: "Harper Lee",
-    year: 1960,
-  },
-  {
-    title: "The Hobbit",
-    author: "J.R.R. Tolkien",
-    year: 1937,
-  },
-  {
-    title: "Brave New World",
-    author: "Aldous Huxley",
-    year: 1932,
-  },
+  new Book("1984", "George Orwell", 1949),
+  new Book("The Great Gatsby", "F. Scott Fitzgerald", 1925),
+  new Book("To Kill a Mockingbird", "Harper Lee", 1960),
+  new Book("The Hobbit", "J.R.R. Tolkien", 1937),
+  new Book("Brave New World", "Aldous Huxley", 1932),
 ];
 const booksEl = document.querySelector(".books");
-
-function Book() {
-  this.id = crypto.randomUUID();
-}
+const newBookDialog = document.querySelector("#new-book");
 
 function resetBooksDisplay() {
   booksEl.innerHTML = "";
 }
 function displayBooks() {
   resetBooksDisplay();
-  booksEl.innerHTML = LIBRARY.map((book) => `<li>${book.title}</li>`).reduce(
-    (acc, current) => acc + current,
-  );
+  booksEl.innerHTML = LIBRARY.map(
+    (book) => `<li id="${book.id}">${book.title}</li>`,
+  ).reduce((acc, current) => acc + current);
 }
 function addBookToLibrary(book) {
   LIBRARY.push(book);
